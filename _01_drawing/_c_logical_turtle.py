086479d4c673c72ff45354d90a0dfac061c6aa8a
League Student <league@iMac.attlocal.net>
2023-04-28 18:29:17 -0700

    draw pentagon and change to random color everytime.

--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,38 +13,39 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    jack.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
-
+    jack.goto(x, y)
 
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
         # 9. Make the turtle spin 360 degrees using the .right() method
-        
+        jack.right(360)
         # 10. Use the .color() method and getRandomColor() function to change
         # the color of the turtle,
         # myTurtle.color(get_random_color())
-
+        jack.color(get_random_color)
 
 if __name__ == '__main__':
     window = turtle.Screen()
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    jack = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    jack.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    jack.color('green')
+    jack.pencolor('blue')
     # 4. Set and new width, length, and outline of our turtle
     #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    jack.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
     # my_turtle.onclick(turtle_clicked)
-
+    jack.onclick(turtle_clicked)
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     turtle.done()
